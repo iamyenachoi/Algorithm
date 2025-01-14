a05859645878d7c21422d7c55ab1b57d669a7070
yenachoi <128819794+iamyenachoi@users.noreply.github.com>
2025-01-14 23:17:37 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    count1 = 0
+    count2 = 0
+    for i in num_list:
+        if i % 2 == 0:
+            count1 += 1
+        else:
+            count2 += 1
+    answer = [count1, count2]
+    return answer
