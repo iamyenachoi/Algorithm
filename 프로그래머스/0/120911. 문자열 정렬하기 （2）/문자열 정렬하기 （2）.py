50c4273becd92a24112fa2e174552c59fca421a1
yenachoi <128819794+iamyenachoi@users.noreply.github.com>
2025-01-28 05:15:35 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,3 +1,10 @@
 def solution(my_string):
-    answer = ''.join(sorted(my_string.lower()))
+    answer = ''
+    lower = []
+    for i in my_string:
+        if ord(i) >= ord('A') and ord(i) <= ord('Z'):
+            lower.append(chr(ord(i) + 32))
+        else:
+            lower.append(i)
+    answer = ''.join(sorted(lower))
     return answer
