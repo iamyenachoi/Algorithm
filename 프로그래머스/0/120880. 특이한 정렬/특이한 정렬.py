dd3aa4d5952759d5c7a5198382380f8006017848
YENA <128819794+iamyenachoi@users.noreply.github.com>
2025-03-02 05:47:45 +0900

    [level 0] Title: 특이한 정렬, Time: 0.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,3 +1,12 @@
 def solution(numlist, n):
-    answer = sorted(numlist, key=lambda x: (abs(x - n), -x))
+    temp = []
+    for num in numlist:
+        temp.append((abs(num - n), num))
+    for i in range(len(temp)):
+        for j in range(i + 1, len(temp)):
+            if temp[i][0] > temp[j][0] or (temp[i][0] == temp[j][0] and temp[i][1] < temp[j][1]):
+                temp[i], temp[j] = temp[j], temp[i]    
+    answer = []
+    for k in temp:
+        answer.append(k[1])    
     return answer
